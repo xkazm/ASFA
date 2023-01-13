e8c93e5a9035b34b8cdcd921907002a4b30edf79
Kun Xia <1713481436@qq.com>
2023-01-13 16:49:11 +0800

    Update DomainDiscriminator.py

--- libs/DomainDiscriminator.py
+++ libs/DomainDiscriminator.py
@@ -6,7 +6,7 @@ import torch
 
 class DomainDiscriminator(nn.Module):
     """
-    Domain Discriminator Module - 3 layers MLP
+    Domain Discriminator Module - 2 layers MLP
     """
 
     def __init__(self,
@@ -17,8 +17,6 @@ class DomainDiscriminator(nn.Module):
             nn.Linear(input_dim, hidden_dim),
             nn.BatchNorm1d(hidden_dim),
             nn.ELU(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.BatchNorm1d(hidden_dim),
             nn.Linear(hidden_dim, 1),
             nn.Sigmoid()
         )
